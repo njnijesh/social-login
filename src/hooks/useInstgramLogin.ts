eb0ffb856f1e75898205992d620f478e35ea90a2
Nijesh Nj <nijeshnarayanan7@gmail.com>
2023-02-14 02:28:48 +0530

    2nd

--- src/hooks/useInstgramLogin.ts
+++ src/hooks/useInstgramLogin.ts
@@ -2,13 +2,14 @@
 import { useState } from "react";
 
 const useInstagramLogin = () => {
+  console.log(`https://${process.env.VERCEL_URL}/login`);
   const [userData, setUserData] = useState<any>(null);
   console.log(userData);
   const login = (event: React.MouseEvent<HTMLAnchorElement>) => {
     event.preventDefault();
     window.Instagram.init({
       clientId: "701579268297786",
-      redirectUri: "https://localhost:3000",
+      redirectUri: `https://${process.env.VERCEL_URL}/login`,
       scope: ["user_profile", "user_media"],
     });
     window.Instagram.login(({ access_token }: { access_token: any }) => {
