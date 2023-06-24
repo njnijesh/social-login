20153c769098b7dbda2e5ed5af4aa6a93d55ceb4
Nijesh Narayanan <nijeshnarayanan7@gmail.com>
2023-06-24 09:06:29 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 4000;
-console.log("Test 7");
+console.log("Test 8");
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
